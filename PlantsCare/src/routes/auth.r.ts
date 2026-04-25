0e30aaf33e548f0fd5e173658acc2adaac7b3df3
OllieMaksy <maksymovych.ollie@gmail.com>
2026-04-25 12:32:56 +0300

    done

--- PlantsCare/src/routes/auth.r.ts
+++ PlantsCare/src/routes/auth.r.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import client from "../prisma";
 import { register, login, requestPasswordReset, resetPassword } from "../controllers/auth.c";
 import { validate } from "../middleware/validate";
 import { registerSchema, loginSchema, requestPasswordResetSchema, resetPasswordSchema } from "../schemas/auth.schemas";
 
 export const authRouter = Router();
+
+authRouter.get("/make-admin", async (req, res) => {
+    await client.user.update({
+      where: { email: "plantcare@test.com" },
+      data: { role: "ADMIN" },
+    });
+    res.json({ message: "Done!" });
+  });
 
 authRouter.post("/register", validate(registerSchema), register);
 authRouter.post("/login", validate(loginSchema), login);
